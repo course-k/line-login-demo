efcb8d1b76c7d51ca8813b56ecde12f4afb3f5ab
course-k <course.k.ky@gmail.com>
2024-04-09 06:22:36 +0900

    adjustment

--- app.js
+++ app.js
@@ -1,19 +1,45 @@
-import express from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import crypto from "crypto";
+import dotenv from "dotenv";
 
 const PORT = 3000;
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const app = express();
+dotenv.config();
 
-app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname,'views','login.html'));
+const createRedirectUrl = (state) => {
+  const redirectUrl =
+    "https://access.line.me/oauth2/v2.1/authorize?" +
+    "response_type=code" +
+    `&client_id=${process.env.CHANNEL_ID}` +
+    `&redirect_uri=${process.env.CALLBACK_URL}` +
+    `&state=${state}` +
+    "&scope=profile%20openid";
+  return redirectUrl;
+};
+
+function getToken() {
+  const bytes = crypto.randomBytes(32);
+  const token = bytes.toString("base64url");
+  return token;
+}
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "login.html"));
 });
 
-app.get('/login', (req,res)=>{
-    res.send('login');
+app.get("/login", (req, res) => {
+  const state = getToken();
+  const redirect = createRedirectUrl(state);
+  res.redirect(redirect);
 });
 
-app.listen(PORT,()=>{
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.get("/success", (req, res) => {
+  res.send(res.status);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
